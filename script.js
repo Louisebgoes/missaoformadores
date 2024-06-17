c5ad3254ccc4b6b00d0de5c57f7b9e9a318b45bb
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-06-17 14:36:26 -0300

    perguntas

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 { 
-    enunciado : "Pergunta 1",
+    enunciado : "Como você acredita que podemos combater o racismo na sociedade?",
     alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
+        "Participando de campanhas de conscientização e educação sobre igualdade racial.",
+        "Denunciando atos de discriminação e apoiando políticas públicas inclusivas."
     ] 
 },
 { 
-    enunciado : "Pergunta 2",
+    enunciado : "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
     alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
+        "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
+        "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo"
     ] 
 },
 { 
-    enunciado : "Pergunta 3",
+    enunciado : "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
     alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
+        "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
+        "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda."
     ] 
 }
 
