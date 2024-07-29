30e58b22830d4735e6f0a79870317f63b165e983
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-07-29 14:13:56 -0300

    afirmacao

--- script.js
+++ script.js
@@ -8,22 +8,34 @@ const perguntas = [
 { 
     enunciado : "Como você acredita que podemos combater o racismo na sociedade?",
     alternativas: [
-        "Participando de campanhas de conscientização e educação sobre igualdade racial.",
-        "Denunciando atos de discriminação e apoiando políticas públicas inclusivas."
+        {texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
+        afirmacao:"Afirmação 1 "
+        },
+        {texto: "Denunciando atos de discriminação e apoiando políticas públicas inclusivas.",
+        afirmacao:"afirmacao 2"
+        }
     ] 
 },
 { 
     enunciado : "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
     alternativas: [
-        "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
-        "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo"
+        {texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
+            afirmacao: "afirmação 3"
+        },
+        {texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo",
+        afirmacao: "afirmação 4"
+        }
     ] 
 },
 { 
     enunciado : "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
     alternativas: [
-        "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
-        "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda."
+        {texto: "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
+            afirmacao: "afirmação 5"
+        },
+        {texto: "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda.",
+            afirmacao: "afirmação 6"
+        }
     ] 
 }
 
